d558b578a2a19fe39c9f6ecd6a901a6e2a9a519c
Ahmed Nabil <a.ahmed.nabil90@gmail.com>
2024-07-28 04:34:33 +0300

    remove old core lib dependency add update,review libraries [compatible with sdk 34]

--- app/src/main/java/com/islam/hesn/myapplication/home/MainActivity.kt
+++ app/src/main/java/com/islam/hesn/myapplication/home/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.gms.tasks.Task
 import com.google.android.play.core.appupdate.AppUpdateInfo
 import com.google.android.play.core.appupdate.AppUpdateManager
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
@@ -21,7 +22,6 @@ import com.google.android.play.core.install.model.UpdateAvailability
 import com.google.android.play.core.review.ReviewInfo
 import com.google.android.play.core.review.ReviewManager
 import com.google.android.play.core.review.ReviewManagerFactory
-import com.google.android.play.core.tasks.Task
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.islam.hesn.myapplication.R
 import com.islam.hesn.myapplication.databinding.ActivityMainBinding
